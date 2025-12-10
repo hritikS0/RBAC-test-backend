f7af4826165e79444f95ff0039432b5837e1babd
Hritik Sharma <hritiksharma@Hritiks-MacBook-Pro.local>
2025-12-10 16:56:14 +0530

    fixed vercel error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
 import UserRouter from "./routes/user.router.js";
 import ProjectRouter from "./routes/project.router.js";
 import connectDb from "./db/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
